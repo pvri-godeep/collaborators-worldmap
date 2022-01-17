06e3740ff870bfd4b16fa7a9c1fe95dcd6ad4b48
WorkPhil <Philipp.Krieb@innere.med.uni-giessen.de>
2022-01-17 15:33:42 +0100

    Update process.sh
    Auf Git-Variablen konzentriert

--- scripts/process.sh
+++ scripts/process.sh
@@ -1,7 +1,11 @@
+#!/bin/sh 
+
+
+
 python geocoding.py
 
-Rscript worldMap.R
-Rscript legend.R
+"$R_HOME"/bin/Rscript --no-save --no-restore worldMap.R
+"$R_HOME"/bin/Rscript --no-save --no-restore legend.R
 
 python imgCrop.py
 python imgPaste.py
