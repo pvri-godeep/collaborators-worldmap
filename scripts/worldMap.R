d896241aa4bc78a92f5a257518dafdbd5b4fdecd
WorkPhil <Philipp.Krieb@innere.med.uni-giessen.de>
2022-04-27 14:41:35 +0200

    Update worldMap.R
    Pending neu, mit eigener Farbe

--- scripts/worldMap.R
+++ scripts/worldMap.R
@@ -14,11 +14,21 @@ map('world',
     border="lightskyblue",
     ylim = c(-60, 90))
 
-points(coordTable$Longitude,
-       coordTable$Latitude, 
+
+# etl_status - connected
+points(coordTable$Longitude[coordTable$etl_status == 'connected'],
+       coordTable$Latitude[coordTable$etl_status == 'connected'], 
        pch=20, 
        cex=2, 
-       col="red")  
+       col="red")
+
+# etl_status - pending
+points(coordTable$Longitude[coordTable$etl_status == 'pending'],
+       coordTable$Latitude[coordTable$etl_status == 'pending'], 
+       pch=20, 
+       cex=2, 
+       col="pink")
+
 
 # legend("bottomleft", 
 #        c("PH centers connected to GoDeep","Ethical approval pending"), 
