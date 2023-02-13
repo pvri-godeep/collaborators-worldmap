08f1745005d3fe0e319ab84b7d65475b09626dcc
R.W.Majeed <rwmajeed@gmail.com>
2023-02-13 21:59:51 +0100

    use caching of previously written output so we don't need to lookup every city every time.

--- scripts/geocoding.py
+++ scripts/geocoding.py
@@ -9,16 +9,28 @@ app = Nominatim(user_agent="tutorial") # instantiate a new Nominatim client
 
 
 dirname = os.path.dirname(__file__)
-input   = os.path.join(dirname, '../../reporting/collections-metadata.csv')
+input   = os.path.join(dirname, '../../reporting/collections-metadata.tsv')
 output  = os.path.join(dirname, '../target/coords.csv')
 
-
 header = ['City', 'Latitude', 'Longitude','etl_status']
-csvdata = []
 
+# Cache coordinates previously written to output
+coordcache = {}
+with open(output, newline='') as csvfile:
+    reader = csv.DictReader(csvfile, delimiter=';')
+    for row in reader:
+        coordcache[row[header[0]]] = (row[header[1]],row[header[2]])
+
+
+csvdata = []
 
 # This function returns a location as raw from an address will repeat until success
 def get_location_by_address(address):
+    # before any internet calls, check cache first
+    if address in coordcache:
+        latlon = coordcache[address]
+        return {'lat':latlon[0], 'lon':latlon[1]}
+    
     time.sleep(1) # Wichtig - nicht löschen
     try:
         return app.geocode(address).raw
@@ -28,7 +40,7 @@ def get_location_by_address(address):
 
 # Hiermit werden die Koordianten mit hilfe des IMputs erstellt und für die Ausgabe vorbereitet
 with open(input) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=';')
+    csv_reader = csv.reader(csv_file, delimiter='\t')
     line_count = 0
     for row in csv_reader:
         if line_count != 0:
