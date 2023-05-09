f333ceb80298d566258c1677eebdc13dfee413a9
R.W.Majeed <rwmajeed@gmail.com>
2023-05-09 10:32:28 +0200

    use env INKSCAPE_HOME

--- scripts/process.sh
+++ scripts/process.sh
@@ -1,9 +1,11 @@
 #!/bin/sh 
 
-inkscape="/c/Program Files/Inkscape/bin/inkscape.exe"
+set -e
+
+inkscape="bin/inkscape.exe"
 
 python geocoding.py
 
 "$R_HOME"/bin/Rscript --no-save --no-restore worldmap-svg.R
 
-"$inkscape" --batch-process --export-filename=../target/worldmap-latest-800.png --export-height=800 --export-area=70:135:800:445 ../target/worldmap-latest.svg
+"$INKSCAPE_HOME"/bin/inkscape --batch-process --export-filename=../target/worldmap-latest-800.png --export-height=800 --export-area=70:135:800:445 ../target/worldmap-latest.svg
