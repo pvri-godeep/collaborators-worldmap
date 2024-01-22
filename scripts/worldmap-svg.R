2a16684ff086cac1583bfaf96a43f127dfe60210
R.W.Majeed <rwmajeed@gmail.com>
2024-01-22 16:18:00 +0100

    generate additional map for website

--- scripts/worldmap-svg.R
+++ scripts/worldmap-svg.R
@@ -5,8 +5,6 @@ coordTable  <- read.table("../target/coords.csv",
                           header = TRUE, 
                           sep=";")
 
-#png(filename = "../target/wolrdMap.png", width = 600)
-
 # for SVG change remove PNG above and uncomment the following line
 svg(filename="../target/worldmap-latest.svg", width=9, height=6)
 
@@ -19,7 +17,7 @@ map('world',
 
 
 
-# etl_status - pending
+# etl_status - talks
 points(coordTable$Longitude[coordTable$etl_status == 'talks'],
        coordTable$Latitude[coordTable$etl_status == 'talks'], 
        pch=20, 
@@ -47,5 +45,42 @@ points(coordTable$Longitude[coordTable$etl_status == 'connected'],
 #        c("PH centers connected to GoDeep","Ethical approval pending"), 
 #        fill = c("red","indianred1"), 
 #        bty = "n")
+
+dev.off()
+
+
+# generate map for web site
+svg(filename="../target/worldmap-web-latest.svg", width=9, height=6)
+
+map('world',
+    col="#b6e0fc", # lightskyblue
+    fill=TRUE,
+    border="#b6e0fc",
+    ylim = c(-60, 90))
+
+
+
+# etl_status - talks
+points(coordTable$Longitude[coordTable$etl_status == 'talks'],
+       coordTable$Latitude[coordTable$etl_status == 'talks'], 
+       pch=20, 
+       cex=1.5, 
+       col="slategray")
+       
+# etl_status - pending
+points(coordTable$Longitude[coordTable$etl_status == 'pending' | coordTable$etl_status == 'etl'],
+       coordTable$Latitude[coordTable$etl_status == 'pending' | coordTable$etl_status == 'etl'], 
+       pch=20, 
+       cex=1.5, 
+       col="red")
+
+# etl_status - connected
+points(coordTable$Longitude[coordTable$etl_status == 'connected'],
+       coordTable$Latitude[coordTable$etl_status == 'connected'], 
+       pch=20, 
+       cex=1.5, 
+       col="red")
+
+legend(x=-40,y=-35, border="white", c("PH centers connected to GoDeep","Ethical approval pending"), fill = c("red","slategray"), bty = "n")
 
 dev.off()
